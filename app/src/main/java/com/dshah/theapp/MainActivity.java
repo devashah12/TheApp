884438ec59c3c29a67c42a10a238382eea31fb57
Deva Shah <dshah@hcn-inc.com>
2018-04-03 15:02:53 -0400

    Initial commit

---
+++ app/src/main/java/com/dshah/theapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dshah.theapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        webView = (WebView) findViewById( R.id.webView );
+        webView.getSettings().setJavaScriptEnabled( true );
+        webView.loadUrl( "file:///android_asset/build/index.html" );
+
+
+    }
+}
